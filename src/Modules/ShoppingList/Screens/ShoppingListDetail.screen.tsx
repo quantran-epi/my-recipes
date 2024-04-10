3b3308136f1f11c9948b21178887151f5aaef177
quantd <quan.td@shb.com.vn>
2024-04-10 12:21:20 +0700

    update

--- src/Modules/ShoppingList/Screens/ShoppingListDetail.screen.tsx
+++ src/Modules/ShoppingList/Screens/ShoppingListDetail.screen.tsx
@@ -1,24 +1,51 @@
+import { Button } from "@components/Button";
+import { Collapse } from "@components/Collapse";
 import { Checkbox } from "@components/Form/Checkbox";
 import { List } from "@components/List"
 import { Typography } from "@components/Typography";
+import { RootRoutes } from "@routing/RootRoutes";
 import { ShoppingList, ShoppingListIngredientGroup } from "@store/Models/ShoppingList"
 import { toggleDoneIngredient } from "@store/Reducers/ShoppingListReducer";
 import { RootState } from "@store/Store";
 import { CheckboxChangeEvent } from "antd/es/checkbox";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 type ShoppingListDetailScreenProps = {
     shoppingList: ShoppingList;
 }
 
 export const ShoppingListDetailScreen: React.FunctionComponent<ShoppingListDetailScreenProps> = (props) => {
+    const navigate = useNavigate();
+    const dishes = useSelector((state: RootState) => state.dishes.dishes);
 
-    return <List
-        itemLayout="horizontal"
-        dataSource={props.shoppingList.ingredients}
-        renderItem={(item) => <ShoppingListIngredientItem item={item} shoppingList={props.shoppingList} />}
-    />
+    const _getDishesByIds = (ids: string[]) => {
+        return dishes.filter(e => ids.includes(e.id));
+    }
+
+    return <React.Fragment>
+        <Collapse
+            size="small"
+            items={[{
+                key: 'dishes', label: 'Dishes ' + `(${props.shoppingList.dishes.length})`, children: <List
+                    dataSource={_getDishesByIds(props.shoppingList.dishes)}
+                    renderItem={(item) => <List.Item>
+                        <Button type="link" onClick={() => navigate(RootRoutes.AuthorizedRoutes.DishesRoutes.ManageIngredient(item.id))}>
+                            {item.name} ({item.ingredients.length} ingredients)
+                        </Button>
+                    </List.Item>
+                    }
+                />
+            }]}
+        />
+        <List
+            itemLayout="horizontal"
+            dataSource={props.shoppingList.ingredients}
+            renderItem={(item) => <ShoppingListIngredientItem item={item} shoppingList={props.shoppingList} />}
+        />
+    </React.Fragment>
+
 }
 
 type ShoppingListIngredientItemProps = {
@@ -52,7 +79,7 @@ export const ShoppingListIngredientItem: React.FunctionComponent<ShoppingListIng
             actions={
                 [
                 ]
-            }>
+            } >
             <List.Item.Meta
                 avatar={<Checkbox defaultChecked={props.item.isDone} onChange={_onCheckedChange} />}
                 title={<Typography.Text type={props.item.isDone ? "secondary" : undefined} style={{ textDecorationLine: props.item.isDone ? "line-through" : "none" }}>{_getIngredientNameById(props.item.ingredientId)}</Typography.Text>}
@@ -62,5 +89,5 @@ export const ShoppingListIngredientItem: React.FunctionComponent<ShoppingListIng
                         <Typography.Text>{item.amount} {item.unit} ({_getDishesNameById(item.dishesId)})</Typography.Text>
                     </List.Item>} />} />
         </List.Item >
-    </React.Fragment>
+    </React.Fragment >
 }
